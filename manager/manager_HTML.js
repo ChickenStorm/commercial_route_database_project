352c1a3734fb236a80b4afef34830146f39d115c
ChickenStorm <tetras.aigle@yahoo.fr>
2015-03-05 18:32:49 +0100

    no message

--- manager/manager_HTML.js
+++ manager/manager_HTML.js
@@ -1,5 +1,8 @@
 
 
+
+var factionColorArray;
+
 function getHTMLTable(arrayToDisapay,arrayStyle) { // ceci prend deux array 2-dimentionelle et retourne un tableau en html
     var tempTextTable = "<table style='background-color :white' >";
     
@@ -29,6 +32,8 @@ function drawHTMLPage(){
     var HTMLText = " <div style =' position:relative;text-align: center;display: inline-block; ' id='generalInfos' > Version "+ version +" </div> <div style =' position:absolute;text-align: center;display: inline-block;' id='author' > </div>"
     
     HTMLText += " <div style =' position:absolute;display: inline-block; ' id='liste1Container' > </div> <div style =' position:absolute;display: inline-block; ' id='liste2Container' > </div> "
+    HTMLText += "<div style =' position:absolute;display: inline-block; ' id='commRoutesTable' > </div>"
+    
     
     $("body").innerHTML += HTMLText;
     
@@ -36,9 +41,69 @@ function drawHTMLPage(){
     // position
 }
 
+function getFactionName(id){
 
+    if (id==1) { // i don't like switch
+        return "Empire"
+    }
+    else if(id==2){
+        return "Kovahk"
+    }
+    else if(id==3){
+        return "N&eacute;gore"
+    }
+    else if(id==4){
+        return "Cardan"
+    }
+    else if(id==5){
+        return "Nerve"
+    }
+    else if(id==6){
+        return "Aph&eacute;ra"
+    }
+    else if(id==7){
+        return "Synelle"
+    }
+    else if (id==undefined) {
+        return "?"
+    }
+    else{
+        return "rebelle";
+    }
+    
+}
 
-function drawInterface(){
+function getFactionColor(id){
+    if (id==1) { // i don't like switch
+        return "red"
+    }
+    else if(id==2){
+        return "blue"
+    }
+    else if(id==3){
+        return "yellow"
+    }
+    else if(id==4){
+        return "purple"
+    }
+    else if(id==5){
+        return "green"
+    }
+    else if(id==6){
+        return "cyan"
+    }
+    else if(id==7){
+        return "orange"
+    }
+    else{
+        return "grey";
+    }
+}
+
+function drawInterfaceInit(){
+   
+    
+    
     $("generalInfos").style.top = "20px";
     $("generalInfos").style.left = "300px";
     $("generalInfos").innerHTML = "<p style = 'font-size:  20px;' >Routes commercial project. <br>Version "+version +"  <p style='color: black' style='display: block;'> Avertissement : les donn&eacute;e fournies ne sont pas garanties &ecirc;tre juste ni &agrave; jour.</p>";
@@ -54,17 +119,28 @@ function drawInterface(){
     $("liste1Container").style.left ="100px"
     $("liste1Container").style.top = "350px"
     //$("liste1Container").style.width = "500px"
-    $("liste1Container").innerHTML = "<input id=input1> <button onclick='searchListe1()'> cherch&eacute;</button> : nom du joueur <input type=checkBox id=checkBox1> Regular expression"
+    $("liste1Container").innerHTML = "<input id=input1> <button onclick='searchListe1()'> cherch&eacute;</button> : <select style=';' id='field1'> <option value='playerName'>nom du joueur</option> <option value='name'>nom</option> </select>   <input type=checkBox id=checkBox1> Regular expression <div id=list1></div>"
     
-    $("liste2Container").style.left ="1000px"
+    $("liste2Container").style.left ="700px"
     $("liste2Container").style.top = "350px"
     //$("liste2Container").style.width = "10px"
-    $("liste2Container").innerHTML = "<input id=input2>  <button onclick='searchListe2()'> cherch&eacute;</button> : nom du joueur <input type=checkBox id=checkBox2> Regular expression"
+    $("liste2Container").innerHTML = "<input id=input2>  <button onclick='searchListe2()'> cherch&eacute;</button> : <select style=';' id='field2'> <option value='playerName'>nom du joueur</option> <option value='name'>nom</option> </select>  <input type=checkBox id=checkBox2> Regular expression <div id=list2></div>"
     
-    drawPlanetList($("liste1Container"),planetArray1);
-    drawPlanetList($("liste2Container"),planetArray2);
+    
+    $("commRoutesTable").style.left ="1400px";
+    $("commRoutesTable").style.top = "350px";
+    // drawPlanetList($("list1"),planetArray1);
+    // drawPlanetList($("list2"),planetArray2);
+    
+    drawInterface();
 }
 
+function drawInterface(){
+    drawPlanetList($("list1"),planetArray1);
+    drawPlanetList($("list2"),planetArray2);
+    
+    darwCommRouteTable($("commRoutesTable"),commRoutesArray)
+}
 
 function drawPlanetList(listeContainer,array){
     
@@ -77,14 +153,30 @@ function drawPlanetList(listeContainer,array){
     
     for (var i in array){
         
-        arrayToDraw.push([array[i]["name"],array[i]["playerName"],array[i]["posX"],array[i]["posY"],array[i]["posZ"],array[i]["secteur"],array[i]["faction"],array[i]["id"]]);
-        styleArrayToDraw.push(["","","","","","","",""]);
+        arrayToDraw.push([array[i]["name"],array[i]["playerName"],array[i]["posX"],array[i]["posY"],array[i]["posZ"],array[i]["secteur"],getFactionName(array[i]["faction"]),array[i]["id"]]);
+        styleArrayToDraw.push(["","","","","","","background-color :"+getFactionColor(array[i]["faction"]),""]);
         
         
         
         
     }
     
-    listeContainer.innerHTML += getHTMLTable(arrayToDraw,arrayToDraw);
+    listeContainer.innerHTML = getHTMLTable(arrayToDraw,styleArrayToDraw);
     
+}
+
+
+function darwCommRouteTable(container,array){
+    var arrayToDraw = [["joueur 1","joueur 2","base 1" ,"base 2","distance","revenu","pop total"]];
+    var styleArrayToDraw = [["","","","","","","",""]];
+    
+    for (var i in array){
+        
+        arrayToDraw.push([array[i].p1Name,array[i].p2Name,array[i].b1Name,array[i].b2Name,array[i].dist,array[i].income,array[i].pop]);
+        styleArrayToDraw.push(["","","","","",""]);
+        
+        
+    }
+    
+    container.innerHTML = "<button onclick='computeCommRoutes()'> calcul&eacute; </button>"+getHTMLTable(arrayToDraw,styleArrayToDraw)
 }
--- manager/manager_main.js
+++ manager/manager_main.js
@@ -15,6 +15,8 @@ var planetArrayGlobal = [planetArray1,planetArray2] // all is by ref
 
 var getFBDataTimout;
 
+var commRoutesArray = [];
+
 function Planet(factionNumber,name,playerName,posX,posY,posZ,secteur){
     
     this["faction"] = factionNumber;
@@ -26,6 +28,20 @@ function Planet(factionNumber,name,playerName,posX,posY,posZ,secteur){
     this["secteur"] = secteur;
 }
 
+function CommRoute(name1,name2,name3,name4,dits,income,pop,idf1,idf2,b1id,b2id) {
+    this.p1Name = name1;
+    this.p2Name = name2;
+    this.b1Name = name3;
+    this.b2Name = name4;
+    this.dist = dist;
+    this.income = income
+    this.pop = pop
+    this.factionId1 = idf1;
+    this.factionId2 = idf2;
+    this.b1id = b1id;
+    this.b2id = b2id;
+}
+
 function managerInit(){
     
     
@@ -33,7 +49,8 @@ function managerInit(){
     //getDataFormFireBase()
     
     drawHTMLPage();
-    drawInterface();
+    drawInterfaceInit();
+    
     
 }
 
@@ -85,11 +102,11 @@ function searchListe1(){
 
 function searchListe1Callback(snapshot){
     
-    
-    planetArray1 = searchListe(snapshot.val()["planetData"],["playerName"],[$("input1").value],$("checkBox1").checked);
+    //alert($("field1").value)
+    planetArray1 = searchListe(snapshot.val()["planetData"],[$("field1").value],[$("input1").value],$("checkBox1").checked);
    
     
-    drawInterface()
+    drawInterface();
 }
 
 function searchListe2(){
@@ -101,9 +118,9 @@ function searchListe2(){
 }
 
 function searchListe2Callback(snapshot){
-    planetArray2 = searchListe(snapshot.val()["planetData"],["playerName"],[$("input2").value],$("checkBox2").checked);
+    planetArray2 = searchListe(snapshot.val()["planetData"],[$("field2").value],[$("input2").value],$("checkBox2").checked);
     
-    drawInterface()
+    drawInterface();
 }
 
 function searchListe(list,fieldNameArray,searchTextArray,doRegExpInput,arrayToSave){ 
@@ -135,7 +152,9 @@ function searchListe(list,fieldNameArray,searchTextArray,doRegExpInput,arrayToSa
         }
         else{
             for (var j=0;j < searchTextArray.length ; ++j) {
-                if (list[i][fieldNameArray[j]].search(searchTextArray[j]) == -1 ) {
+                
+                
+                if (list[i][fieldNameArray[j]] == undefined || list[i][fieldNameArray[j]].search(searchTextArray[j]) == -1 ) {
                     matchSearch = false;
                 }
             }
@@ -151,4 +170,28 @@ function searchListe(list,fieldNameArray,searchTextArray,doRegExpInput,arrayToSa
     
     return arrayToSave;
     
+}
+
+function computeCommRoutes(){
+    commRoutesArray = computeCommRoutesFormArray(planetArray1,planetArray2)
+}
+
+function computeCommRoutesFormArray(a1,a2){
+    var returnRouteCommArray = []
+    for (var i in a1){
+        for (var j in a2){
+            
+            if (a1[i]["id"]!= a2[j]["id"]) {
+                returnRouteCommArray.push(new CommRoute(a1[i]["playerName"],a2[j]["playerName"],a1[i]["name"],a1[j]["name"],getDist(a1[i],a2[j]),getIncome(a1[i],a2[j]),getPop(a1[i],a2[j]),a1[i]["faction"],a2[j]["faction"],a1[i]["id"], a2[j]["id"]))
+            }
+            
+        }
+    }
+    
+    return returnRouteCommArray;
+    
+}
+
+function getDist(pla1,pla2){
+    return (pla1["posX"]-pla2["posX"] ) + (pla1["posY"]-pla2["posY"])
 }
